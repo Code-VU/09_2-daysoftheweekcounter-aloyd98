34717ff92ea158e6af1327b1a9b5ccd1b1570903
Austin Loyd <Austin.Loyd@vu.com>
2023-05-18 15:57:36 -0500

    redone

--- counter.py
+++ counter.py
@@ -1,6 +1,22 @@
 def countDayOfTheWeek():
     # This first line is provided for you
     file_name = input("Enter a file name: ")
+    han = open(file_name)
+    day_count = {}
+
+    for line in han:
+        if line.startswith('From '):
+            day = return_day(line)
+            day_count[day] = day_count.get(day, 0) + 1
+    
+    print(day_count)
+
+def return_day(line):
+    line = line.strip()
+    line_list = line.split()
+    day = line_list[2]
+
+    return day
 
 
 
